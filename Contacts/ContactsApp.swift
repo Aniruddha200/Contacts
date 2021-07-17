951218ef342b6cf32117ef6979bf88fe392368ad
Aniruddha <aniruddha200baidya@protonmail.com>
2021-07-17 12:13:30 +0100

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,20 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by administrator on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
